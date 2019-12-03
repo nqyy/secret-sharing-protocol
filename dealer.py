3813a15f3b6fe2726bd37ee2d27ff818f42c1743
Stanley Yang <xyang70@illinois.edu>
2019-12-03 02:05:07 -0600

    send IPs work,need to add append data in ss.

--- dealer.py
+++ dealer.py
@@ -1,8 +1,10 @@
 import socket
 import sys
-import json
+import pickle
+import math
 class dealer:
-    def __init__(self,peer_ip_file):
+    def __init__(self,peer_ip_file,buffer_size=1024):
+        self.buffer_size = buffer_size
         self.peer_ip_file = peer_ip_file
         self.peer_ip = list()
         self.__parse_peer_ip()
@@ -12,7 +14,7 @@ class dealer:
         self.secret = secret
     
     def send_secret_to_peers(self):
-        buffer_size = 1024
+
         for peer in self.peer_ip:
             tcp_ip = peer['ip']
             tcp_port = peer['port']
@@ -25,26 +27,31 @@ class dealer:
                 print(type(data))
                 print(data)
                 s.sendall(data)
-                ack = s.recv(buffer_size).decode('utf-8')
+                ack = s.recv(self.buffer_size).decode('utf-8')
                 print('Received ACK: ' + ack)
             finally:
                 print("Closing connection with {}:{}".format(tcp_ip,str(tcp_port)))
                 s.close()
 
     def send_peers_ip(self):
-        buffer_size = 1024
-        peer_ip_temp = json.dumps(self.peer_ip)
+        #self.buffer_size = 1024
+        peer_ip_temp = pickle.dumps(self.peer_ip)
+        print(len(peer_ip_temp))
+        total_pack = math.ceil(len(peer_ip_temp)/self.buffer_size)
         for peer in self.peer_ip:
             tcp_ip = peer['ip']
             tcp_port = peer['port']
 
             try:
+                cur_pack = 0
                 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
                 print("Connecting to {} port {}".format(tcp_ip,str(tcp_port)))
                 s.connect((tcp_ip,tcp_port))
-                s.send(peer_ip_temp)
-                ack = s.recv(buffer_size).decode('utf-8')
-                print('Received ACK: ' + ack)
+                s.sendall(peer_ip_temp)
+                while cur_pack < total_pack:
+                    ack = s.recv(self.buffer_size).decode('utf-8')
+                    print('Received ACK: ' + ack)
+                    cur_pack+=1
             finally:
                 print("Closing connection with {}:{}".format(tcp_ip,str(tcp_port)))
                 s.close()
@@ -65,6 +72,7 @@ class dealer:
 if __name__ == "__main__":
     d = dealer('peer_ip.txt')
     d.make_secret('alkdfjlakdjflkdjfkladsjflkadjflkadjflkadjfldksjflasdkfjadlkfdflkjadklfjalkfdjlkadfadflakdfjlakdjflkdajflksjadflkjsakljdf')
-    d.send_secret_to_peers()
+    #d.send_secret_to_peers()
+    d.send_peers_ip()
 
     
--- node.py
+++ node.py
@@ -1,12 +1,13 @@
 import socket
-import json
+import pickle
 
 class node:
-    def __init__(self,ip,port):
+    def __init__(self,ip,port,buffer_size=1024):
+        self.buffer_size = buffer_size
         self.ip = ip
         self.port = port
         self.secret = 0
-        self.pee_ip = list()
+        self.peer_ip = list()
 
     def receive_secret(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,10 +21,10 @@ class node:
             print('Connection from ',dealer_address)
             counter = 0
             while True:
-                data = connection.recv(1024)
+                data = connection.recv(self.buffer_size)
                 if data:
                     counter+=1
-                    res = 'ACK ' + str(counter)
+                    res = str(counter)
                     res = res.encode('utf-8')
                     print(data.decode('utf-8'))
                     print('Sending ACK back to dealer: ',dealer_address)
@@ -37,8 +38,43 @@ class node:
 
 
     def receive_peer_ip(self):
-        pass
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (self.ip, self.port)
+        print('starting up on %s port %s\n' % server_address)
+        sock.bind(server_address)
+        sock.listen(1)
+        print('Waiting on dealer...')
+        connection, dealer_address = sock.accept()
+        
+        try:
+            print('Connection from ',dealer_address)
+            counter = 0
+            data = None
+            ip_data = []
+            while True:
+                data = connection.recv(self.buffer_size)
+                if data:
+                    counter+=1
+                    res = 'ACK ' + str(counter)
+                    res = res.encode('utf-8')
+                    #print(pickle.loads(data))
+                    ip_data.append(data)
+                    print('Sending ACK back to dealer: ',dealer_address)
+                    connection.send(res)
+                else:
+                    print('Receive end.')
+                    break
+
+        except IOError as e:
+                ip_data.append(data)
+                print('client close too early!')
+        finally:
+            self.peer_ip = pickle.loads(b"".join(ip_data))
+            print(self.peer_ip)
+            print('closing connection.\n')
+            connection.close()       
 
 if __name__ == "__main__":
     n = node('127.0.0.1',5005)
-    n.receive_secret()
+    #n.receive_secret()
+    n.receive_peer_ip()
