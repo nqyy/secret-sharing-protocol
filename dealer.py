471e4fd006722d84447183430cab8fff2a7bcf6d
Stanley Yang <xyang70@illinois.edu>
2019-12-02 22:14:03 -0600

    added connection, have bug...

---
+++ dealer.py
@@ -0,0 +1,69 @@
+import socket
+import sys
+import json
+class dealer:
+    def __init__(self,peer_ip_file):
+        self.peer_ip_file = peer_ip_file
+        self.peer_ip = list()
+        self.__parse_peer_ip()
+        self.secret = 0
+        
+    def make_secret(self,secret):
+        self.secret = secret
+    
+    def send_secret_to_peers(self):
+        buffer_size = 1024
+        for peer in self.peer_ip:
+            tcp_ip = peer['ip']
+            tcp_port = peer['port']
+
+            try:
+                s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                print("Connecting to {} port {}".format(tcp_ip,str(tcp_port)))
+                s.connect((tcp_ip,tcp_port))
+                data = str(self.secret).encode('utf-8')
+                print(type(data))
+                s.sendall(data)
+                ack = s.recv(buffer_size).decode('utf-8')
+                print('Received ACK: ' + ack)
+            finally:
+                print("Closing connection with {}:{}".format(tcp_ip,str(tcp_port)))
+                s.close()
+
+    def send_peers_ip(self):
+        buffer_size = 1024
+        peer_ip_temp = json.dumps(self.peer_ip)
+        for peer in self.peer_ip:
+            tcp_ip = peer['ip']
+            tcp_port = peer['port']
+
+            try:
+                s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                print("Connecting to {} port {}".format(tcp_ip,str(tcp_port)))
+                s.connect((tcp_ip,tcp_port))
+                s.sendall(peer_ip_temp)
+                ack = s.recv(buffer_size).decode('utf-8')
+                print('Received ACK: ' + ack)
+            finally:
+                print("Closing connection with {}:{}".format(tcp_ip,str(tcp_port)))
+                s.close()
+                
+    def __parse_peer_ip(self):
+        with open(self.peer_ip_file) as f:
+            for line in f:
+                ip,port = line.split(":")
+                temp = dict()
+                temp['ip'] = ip
+                temp['port'] = int(port)
+                self.peer_ip.append(temp)
+            
+    
+
+
+
+if __name__ == "__main__":
+    d = dealer('peer_ip.txt')
+    d.make_secret(123)
+    d.send_secret_to_peers()
+
+    
---
+++ node.py
@@ -0,0 +1,45 @@
+import socket
+import json
+
+class node:
+    def __init__(self,ip,port):
+        self.ip = ip
+        self.port = port
+        self.secret = 0
+        self.pee_ip = list()
+
+    def receive_secret(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (self.ip, self.port)
+        print('starting up on %s port %s' % server_address)
+        sock.bind(server_address)
+        sock.listen(1)
+        while True:
+            print('Waiting on dealer...')
+            connection, dealer_address = sock.accept()
+            try:
+                print('Connection from ',dealer_address)
+                counter = 0
+                while True:
+                    data = connection.recvfrom(1024)
+                    if data:
+                        counter+=1
+                        res = 'ACK ' + str(counter)
+                        res = res.encode('utf-8')
+                        print(data)
+                        print('Sending ACK back to dealer: ',dealer_address)
+                        connection.send(res)
+                    else:
+                        print('Receive end.')
+                        break
+            finally:
+                print('closing connection.')
+                connection.close()
+
+
+    def receive_peer_ip(self):
+        pass
+
+if __name__ == "__main__":
+    n = node('127.0.0.1',5005)
+    n.receive_secret()
